41c1676ab489bb604162ba8a7407f955619ecbeb
GarikD <garikd@outlook.fr>
2022-05-03 21:05:09 +0200

    Add : ValidationPipe at the application level

--- src/users/dto/user-post.dto.ts
+++ src/users/dto/user-post.dto.ts
@@ -1,8 +1,12 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsDefined, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class UserPostDTO {
+  @IsDefined()
   @IsNotEmpty()
+  @IsNumber()
   id: number;
 
+  @IsDefined()
   @IsNotEmpty()
+  @IsString()
   name: string;
 }
