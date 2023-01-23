21d295640b4dac26f1703d1daa90cf969aa98e9b
GarikD <garikd@outlook.fr>
2023-01-23 16:11:05 +0100

    feat: Global HTTP logs

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  app.use(morgan('dev'));
 
   app.useGlobalPipes(new ValidationPipe());
 
--- src/middlewares/logger.middleware.ts
+++
@@ -1,4 +0,0 @@
-// export function logger(req: Request, res: Response, next: NextFunction) {
-//   console.log(`${req.method} request was made at ${req.url}`);
-//   next();
-// }
