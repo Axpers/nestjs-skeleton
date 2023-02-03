54cc297dce71295bd77e8add0ad570327b9e6a90
GarikD <garikd@outlook.fr>
2023-02-03 18:51:26 +0100

    feat: Renamed usersService and created authController

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -17,13 +17,13 @@ import { UserLoginRequest } from './requests/user-login-request.dto';
 @Controller('users')
 export class UserController {
   constructor(
-    private usersService: UserService,
+    private userService: UserService,
     private userControllerReponseAdapter: UserControllerReponseAdapter,
   ) {}
 
   @Get()
   async getUsers(): Promise<UserReponse[]> {
-    const users = await this.usersService.getUsers();
+    const users = await this.userService.getUsers();
     return users.map((user) =>
       this.userControllerReponseAdapter.adaptUser(user),
     );
@@ -31,20 +31,20 @@ export class UserController {
 
   @Get(':userId')
   async getUser(@Param() userIdParam: UserIdParam): Promise<UserReponse> {
-    const user = await this.usersService.getUser(userIdParam.userId);
+    const user = await this.userService.getUser(userIdParam.userId);
     return this.userControllerReponseAdapter.adaptUser(user);
   }
 
   @Delete(':userId')
   async deleteUser(@Param() userIdParam: UserIdParam): Promise<void> {
-    await this.usersService.deleteUser(userIdParam.userId);
+    await this.userService.deleteUser(userIdParam.userId);
   }
 
   @Post('register')
   async createUser(
     @Body() userCreateRequest: UserCreateUpdateRequest,
   ): Promise<void> {
-    await this.usersService.createUser(userCreateRequest);
+    await this.userService.createUser(userCreateRequest);
   }
 
   @Put(':userId/update')
@@ -52,11 +52,11 @@ export class UserController {
     @Param() userIdParam: UserIdParam,
     @Body() userUpdateRequest: UserCreateUpdateRequest,
   ): Promise<void> {
-    await this.usersService.updateUser(userIdParam.userId, userUpdateRequest);
+    await this.userService.updateUser(userIdParam.userId, userUpdateRequest);
   }
 
   @Post('login')
   async login(@Body() userLoginRequest: UserLoginRequest): Promise<boolean> {
-    return await this.usersService.login(userLoginRequest);
+    return await this.userService.login(userLoginRequest);
   }
 }
