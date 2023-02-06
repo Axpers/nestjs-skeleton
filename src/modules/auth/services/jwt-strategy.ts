e4722366ada05a35f8946bdfeddd389d4b6de780
GarikD <garikd@outlook.fr>
2023-02-06 17:57:22 +0100

    feat: Jwt validate return type

--- src/modules/auth/services/jwt-strategy.ts
+++ src/modules/auth/services/jwt-strategy.ts
@@ -1,24 +1,20 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UserReponse } from 'src/modules/user/controllers/responses/user-response.dto';
-import { UserControllerReponseAdapter } from 'src/modules/user/controllers/user-controller-response.adapter';
+import { User } from 'src/modules/user/domain/user';
 import { UserRepository } from 'src/modules/user/domain/user-repository';
 import { JwtPayload } from '../domain/jwt-payload';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private userRepository: UserRepository,
-    private userControllerReponseAdapter: UserControllerReponseAdapter,
-  ) {
+  constructor(private userRepository: UserRepository) {
     super({
       secretOrKey: 'defaultSecret',
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
 
-  async validate(payload: JwtPayload): Promise<UserReponse> {
+  async validate(payload: JwtPayload): Promise<User> {
     const userId = payload.id;
     const user = await this.userRepository.getUserById(userId);
 
@@ -26,7 +22,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       throw new UnauthorizedException();
     }
 
-    const adaptedUser = this.userControllerReponseAdapter.adaptUser(user);
-    return adaptedUser;
+    return user;
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -22,6 +22,6 @@ import { EncryptionService } from 'src/core/services/encryption.service';
     UserControllerReponseAdapter,
     { provide: UserRepository, useClass: UserApiRepository },
   ],
-  exports: [UserRepository, UserControllerReponseAdapter],
+  exports: [UserRepository],
 })
 export class UserModule {}
