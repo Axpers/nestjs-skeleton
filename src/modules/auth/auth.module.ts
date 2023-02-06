c4f06369c40d830d4b2ca66eb6c3e90fe247d61a
GarikD <garikd@outlook.fr>
2023-02-06 18:24:56 +0100

    feat: Global auth guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { EncryptionService } from 'src/core/services/encryption.service';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './controllers/auth.controller';
+import { JwtAuthGuard } from './guards/jwt-auth-guard';
 import { AuthUtilsService } from './services/auth-utils.service';
 import { AuthService } from './services/auth.service';
 import { JwtStrategy } from './services/jwt-strategy';
@@ -20,7 +22,16 @@ import { JwtStrategy } from './services/jwt-strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthUtilsService, EncryptionService, JwtStrategy],
+  providers: [
+    AuthService,
+    AuthUtilsService,
+    EncryptionService,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
