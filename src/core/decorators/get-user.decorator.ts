09c648e30284a5c4d16d014f01c592db2c1902f2
GarikD <garikd@outlook.fr>
2023-02-06 18:05:09 +0100

    feat: GetUser param decorator

---
+++ src/core/decorators/get-user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/modules/user/domain/user';
+
+export const GetUser = createParamDecorator(
+  (_data, context: ExecutionContext): User => {
+    const request = context.switchToHttp().getRequest();
+    return request.user;
+  },
+);
