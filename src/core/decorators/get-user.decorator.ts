5da5b3ea8681aad50071c9876fd4b14fc94a94db
GarikD <garikd@outlook.fr>
2023-02-07 18:38:50 +0100

    feat: RolesGuard

--- src/core/decorators/get-user.decorator.ts
+++ src/core/decorators/get-user.decorator.ts
@@ -4,7 +4,7 @@ import { User } from 'src/modules/user/domain/user';
 export const GetUser = createParamDecorator(
   (_data, context: ExecutionContext): User => {
     const request = context.switchToHttp().getRequest();
-    const { user } = request;
+    const user = request.user as User;
 
     return user;
   },
---
+++ src/core/guards/roles.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { User, UserRole } from 'src/modules/user/domain/user';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    const areThereRequiredRoles = requiredRoles !== undefined;
+    if (!areThereRequiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user as User;
+
+    const isUserAdmin = user.role === 'admin';
+    if (isUserAdmin) {
+      return true;
+    }
+
+    const hasRegularUserAtLeastOneRequiredRole = requiredRoles.some(
+      (role) => user.role === role,
+    );
+    return hasRegularUserAtLeastOneRequiredRole;
+  }
+}
