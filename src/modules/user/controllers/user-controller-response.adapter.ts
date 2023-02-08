eb0f09fcdcd3b1ca983c280261a6d609ca6c5e4d
GarikD <garikd@outlook.fr>
2023-02-08 22:07:59 +0100

    feat: Updated user adapters to match resource's adapters

--- src/modules/user/controllers/user-controller-response.adapter.ts
+++ src/modules/user/controllers/user-controller-response.adapter.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ResourceControllerResponseAdapter } from 'src/modules/resource/controllers/resource-controller-response.adapter';
 import { User } from '../domain/user';
 import { UserResponse } from './responses/user-response.dto';
 
 @Injectable()
 export class UserControllerResponseAdapter {
+  constructor(
+    private readonly resourceControllerResponseAdapter: ResourceControllerResponseAdapter,
+  ) {}
+
   adaptUser(user: User): UserResponse {
+    const resources = user.resources.map((resource) =>
+      this.resourceControllerResponseAdapter.adaptResource(resource),
+    );
+
     return {
       id: user.id,
       role: user.role,
+      resources,
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
--- src/modules/user/repositories/user-repository-response.adapter.ts
+++ src/modules/user/repositories/user-repository-response.adapter.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ResourceEntityResponseAdapter } from 'src/modules/resource/repositories/resource-repository-response.adapter';
 import { User } from '../domain/user';
 import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UserEntityResponseAdapter {
+  constructor(
+    private readonly resourceEntityResponseAdapter: ResourceEntityResponseAdapter,
+  ) {}
+
   adaptUser(user: UserEntity): User {
+    const resources = user.resources.map((resourceEntity) =>
+      this.resourceEntityResponseAdapter.adaptResource(resourceEntity),
+    );
+
     return {
       id: user.id,
       role: user.role,
+      resources,
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
