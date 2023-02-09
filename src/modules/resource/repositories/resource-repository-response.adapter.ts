67d12a5c7988ac3759d8cb7131d5905e03ec6217
GarikD <garikd@outlook.fr>
2023-02-09 18:28:32 +0100

    fix: No user found error status code

--- src/modules/resource/repositories/resource-repository-response.adapter.ts
+++ src/modules/resource/repositories/resource-repository-response.adapter.ts
@@ -5,7 +5,8 @@ import { ResourceEntity } from './entities/resource.entity';
 @Injectable()
 export class ResourceEntityResponseAdapter {
   adaptResource(resource: ResourceEntity): Resource {
-    // Why was it done like this ? Because typeorm return the foreign key instead of the whole user
+    // Why was it done like this ?
+    // Because typeorm return the foreign key instead of the whole user
     const userId = resource.user as unknown as string;
 
     return {
--- src/modules/user/services/user-utils.service.ts
+++ src/modules/user/services/user-utils.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../domain/user-repository';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class UserUtilsService {
     const potentialExistingUser = await this.userRepository.getUserById(userId);
     const isIdAlreadyTaken = potentialExistingUser?.id === userId;
     if (!isIdAlreadyTaken) {
-      throw new BadRequestException(
+      throw new NotFoundException(
         'There is no registered account under that id',
       );
     }
