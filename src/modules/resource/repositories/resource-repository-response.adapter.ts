2a558e20a1bbaff98063bd39a8a37edbb1bb4e28
GarikD <garikd@outlook.fr>
2023-02-09 19:27:01 +0100

    fix: ResourceEntity having bad user format attribute

--- src/modules/resource/repositories/resource-repository-response.adapter.ts
+++ src/modules/resource/repositories/resource-repository-response.adapter.ts
@@ -5,14 +5,10 @@ import { ResourceEntity } from './entities/resource.entity';
 @Injectable()
 export class ResourceEntityResponseAdapter {
   adaptResource(resource: ResourceEntity): Resource {
-    // Why was it done like this ?
-    // Because typeorm return the foreign key instead of the whole user
-    const userId = resource.user as unknown as string;
-
     return {
       id: resource.id,
       description: resource.description,
-      userId,
+      userId: resource.user?.id,
     };
   }
 }
