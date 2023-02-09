ea30a3194e6b6b2b31165ca8e067f3451df30dd8
GarikD <garikd@outlook.fr>
2023-02-09 16:04:47 +0100

    fix: Resource repository response adapter

--- src/modules/resource/repositories/resource-repository-response.adapter.ts
+++ src/modules/resource/repositories/resource-repository-response.adapter.ts
@@ -5,10 +5,13 @@ import { ResourceEntity } from './entities/resource.entity';
 @Injectable()
 export class ResourceEntityResponseAdapter {
   adaptResource(resource: ResourceEntity): Resource {
+    // Why was it done like this ? Because typeorm return the foreign key instead of the whole user
+    const userId = resource.user as unknown as string;
+
     return {
       id: resource.id,
       description: resource.description,
-      userId: resource.user.id,
+      userId,
     };
   }
 }
