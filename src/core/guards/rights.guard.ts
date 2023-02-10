efce9d7ce37d11882318edb125f72b30f6d030c9
GarikD <garikd@outlook.fr>
2023-02-10 17:36:48 +0100

    feat: Support of userId param for RightsGuard

--- src/core/guards/rights.guard.ts
+++ src/core/guards/rights.guard.ts
@@ -1,40 +1,86 @@
 import {
-  Injectable,
   CanActivate,
   ExecutionContext,
+  mixin,
+  Type,
+  Injectable,
   BadRequestException,
   NotFoundException,
 } from '@nestjs/common';
 import { ResourceRepository } from 'src/modules/resource/domain/resource-repository';
 import { User } from 'src/modules/user/domain/user';
+import { RouteParametersType } from '../parameters/routes-parameters';
 
-@Injectable()
-export class RightsGuard implements CanActivate {
-  constructor(private readonly resourceRepository: ResourceRepository) {}
+export const RightsGuard = (
+  ...targetParameters: RouteParametersType[]
+): Type<CanActivate> => {
+  @Injectable()
+  class RightsGuardMixin implements CanActivate {
+    constructor(private readonly resourceRepository: ResourceRepository) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const requestParams = request.params;
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+      console.log(targetParameters);
 
-    const resourceId = requestParams.resourceId;
-    if (resourceId === undefined) {
-      throw new BadRequestException(
-        'resourceId should have been provided as request param',
+      const request = context.switchToHttp().getRequest();
+      const requestParameters = request.params;
+      const requesterUser: User = request.user;
+
+      const isRequesterUserAdmin = requesterUser.role === 'admin';
+      if (isRequesterUserAdmin) {
+        return true;
+      }
+
+      const isUserTargeted = targetParameters.includes('userId');
+      const targetUserId: string | undefined = requestParameters.userId;
+      const hasRightsOnUser = isUserTargeted
+        ? this.hasRightsOnUser(requesterUser, targetUserId)
+        : true;
+
+      const isResourceTargeted = targetParameters.includes('resourceId');
+      const targetResourceId: string | undefined = requestParameters.resourceId;
+      const hasRightsOnResource = isResourceTargeted
+        ? this.hasRightsOnResource(requesterUser, targetResourceId)
+        : true;
+
+      return hasRightsOnUser && hasRightsOnResource;
+    }
+
+    private hasRightsOnUser(
+      requesterUser: User,
+      targetUserId: string | undefined,
+    ): boolean {
+      if (targetUserId === undefined) {
+        throw new BadRequestException(
+          'userId  should have been provided as a request param',
+        );
+      }
+
+      const hasSameId = requesterUser.id === targetUserId;
+      return hasSameId;
+    }
+
+    private async hasRightsOnResource(
+      requesterUser: User,
+      resourceId: string | undefined,
+    ): Promise<boolean> {
+      if (resourceId === undefined) {
+        throw new BadRequestException(
+          'resourceId should have been provided as a request param',
+        );
+      }
+
+      const resource = await this.resourceRepository.getResourceById(
+        resourceId,
       );
-    }
+      if (resource === null) {
+        throw new NotFoundException('Could not find the given resource');
+      }
 
-    const resource = await this.resourceRepository.getResourceById(resourceId);
-    if (resource === null) {
-      throw new NotFoundException('Could not find the given resource');
+      const doesUserHaveRightsOnResource = requesterUser.id === resource.userId;
+      return doesUserHaveRightsOnResource;
     }
-
-    const user: User = request.user;
-    const isUserAdmin = user.role === 'admin';
-    if (isUserAdmin) {
-      return true;
-    }
-
-    const doesUserHaveRightsOnResource = user.id === resource.userId;
-    return doesUserHaveRightsOnResource;
   }
-}
+
+  const guard = mixin(RightsGuardMixin);
+  return guard;
+};
---
+++ src/core/parameters/routes-parameters.ts
@@ -0,0 +1,6 @@
+export enum RouteParameters {
+  ResourceId = 'resourceId',
+  UserId = 'userId',
+}
+
+export type RouteParametersType = `${RouteParameters}`;
